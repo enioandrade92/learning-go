dc96a0e1c7fac6b3ab8d59fcf30da89cf4fb957a
Enio Andrade <enio.goulart@gmail.com>
2024-01-25 14:04:12 -0300

    18-lesson-empty-interface

---
+++ 18-lesson-empty-interface/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+ 
+
+
+func main()  {
+	var x interface{} = 10
+	var y interface{} = "Hello, World!"
+
+	showType(x)
+	showType(y)
+
+}
+
+
+func showType(t interface{}) {
+	fmt.Printf("o tipo da variável é %T e o valor é %v\n", t,t)
+}
+
+
+
+
