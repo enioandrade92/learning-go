61da6df2b0bd56b14aff4ca71437a91373219876
Enio Andrade <enio.goulart@gmail.com>
2024-02-05 15:48:34 -0300

    Import-packages

---
+++ 2-Import-packages/1-manipulation-file/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main()  {
+	// creating file
+	f, err := os.Create("file.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// add string
+	sizeString, err := f.WriteString("Hello world!\n")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File created successfully, size: %d bytes\n", sizeString)
+
+	// writing data
+	sizeByte, err := f.Write([]byte("Writing data\n"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File created successfully, size: %d bytes\n", sizeByte)
+	f.Close()
+
+	// reading file
+	file, err := os.ReadFile("file.txt")
+	if err != nil {
+		panic(err)
+	}
+	// convert from byte to string
+	fmt.Println(string(file))
+
+	// reading in parts
+	readFile, err := os.Open("file.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(readFile)
+	buffer := make([]byte,1)
+
+	for {
+		n, err :=reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+
+}
